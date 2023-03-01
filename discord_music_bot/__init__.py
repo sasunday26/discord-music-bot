507fcb6c110cc960794ea7c091985f920140c2d4
Andrew Yatsura <andrewyazura203@gmail.com>
2023-03-02 01:02:30 +0200

    register error handler on bot start

--- discord_music_bot/__init__.py
+++ discord_music_bot/__init__.py
@@ -1,13 +1,16 @@
+import logging
 from logging.config import dictConfig
 
 import discord
 
 from discord_music_bot import config
 from discord_music_bot.commands import add_cogs
+from discord_music_bot.errors import add_error_handlers
 
 
 def run_bot() -> None:
     dictConfig(config.LOGGING_CONFIG)
+    logger = logging.getLogger("discord_music_bot")
 
     intents = discord.Intents.default()
     intents.message_content = True
@@ -17,6 +20,7 @@ def run_bot() -> None:
         intents=intents,
     )
 
-    add_cogs(bot)
+    add_cogs(bot, logger)
+    add_error_handlers(bot, logger)
 
     bot.run(config.BOT_TOKEN)
--- discord_music_bot/commands/__init__.py
+++ discord_music_bot/commands/__init__.py
@@ -7,9 +7,7 @@ from discord_music_bot.commands.queue import QueueCommands
 from discord_music_bot.commands.streaming import StreamingCommands
 
 
-def add_cogs(bot: discord.Bot) -> None:
-    logger = logging.getLogger("discord_music_bot")
-
+def add_cogs(bot: discord.Bot, logger=logging.Logger) -> None:
     bot.add_cog(AudioCommands(bot=bot, logger=logger))
     bot.add_cog(QueueCommands(bot=bot, logger=logger))
     bot.add_cog(StreamingCommands(bot=bot, logger=logger))
