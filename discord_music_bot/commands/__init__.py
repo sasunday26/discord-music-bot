0d139985ce75f3f548a984a31a92ed01829ff801
Andrew Yatsura <andrewyazura203@gmail.com>
2023-04-06 03:52:52 +0300

    add streaming commands back

--- discord_music_bot/commands/__init__.py
+++ discord_music_bot/commands/__init__.py
@@ -2,11 +2,10 @@ from discord import app_commands
 
 from .audio import add_audio_commands
 from .queue import add_queue_commands
-
-# from .streaming import add_streaming_commands
+from .streaming import add_streaming_commands
 
 
 def add_commands(tree: app_commands.CommandTree) -> None:
     add_audio_commands(tree)
     add_queue_commands(tree)
-    # add_streaming_commands(tree)
+    add_streaming_commands(tree)
