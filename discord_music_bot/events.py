9025f7dc7e1547fbfdf43dd00581e303a76c3433
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2024-07-12 19:12:49 +0300

    On track start, send embed with info about track

--- discord_music_bot/events.py
+++ discord_music_bot/events.py
@@ -2,6 +2,7 @@ import logging
 
 import discord
 import wavelink
+from wavelink import QueueMode, AutoPlayMode
 
 from .client import CustomClient
 
@@ -9,8 +10,39 @@ from .client import CustomClient
 def add_client_events(client: CustomClient, logger: logging.Logger) -> None:
     @client.event
     async def on_wavelink_track_start(
-        payload: wavelink.TrackEventPayload,
+        payload: wavelink.TrackStartEventPayload,
     ) -> None:
+        player: wavelink.Player | None = payload.player
+        if not player:
+            return
+
+        original: wavelink.Playable | None = payload.original
+        track: wavelink.Playable = payload.track
+
+        if player.queue.mode == QueueMode.loop:
+            if not hasattr(player, "last_track"):
+                player.last_track = track
+            elif player.last_track == track:
+                return
+
+        embed: discord.Embed = discord.Embed(
+            title="Now Playing", colour=discord.Colour.random()
+        )
+        embed.description = f"**{track.title}** by `{track.author}`"
+
+        if track.artwork:
+            embed.set_image(url=track.artwork)
+
+        if original and original.recommended:
+            embed.description += (
+                f"\n\n`This track was recommended via {track.source}`"
+            )
+
+        if track.album.name:
+            embed.add_field(name="Album", value=track.album.name)
+
+        await player.home.send(embed=embed)
+
         await client.change_presence(
             activity=discord.Activity(
                 name=payload.track.title,
@@ -21,19 +53,34 @@ def add_client_events(client: CustomClient, logger: logging.Logger) -> None:
 
     @client.event
     async def on_wavelink_track_end(
-        payload: wavelink.TrackEventPayload,
+        payload: wavelink.TrackEndEventPayload,
     ) -> None:
         logger.info(
             f"track {payload.track} finished playing, "
             f"because {payload.reason}"
         )
 
-        if payload.player.queue.is_empty:
+        if (
+            payload.player.queue.mode == QueueMode.loop
+            and payload.reason == "finished"
+        ):
+            await payload.player.play(payload.track)
+            return
+
+        if (
+            payload.player.queue.is_empty
+            and payload.player.autoplay == AutoPlayMode.disabled
+        ):
             await client.change_presence(status=discord.Status.idle)
             return
 
-        if payload.reason == "REPLACED":
+        if payload.reason == "replaced":
             return
+
+        if payload.reason == "stopped" and hasattr(
+            payload.player, "last_track"
+        ):
+            delattr(payload.player, "last_track")
 
         await payload.player.play(payload.player.queue.get())
 
